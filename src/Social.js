8112bb39f590629309d21d14193aef02bed3f545
Elton Solodki-Fransousky <elton.solodki-fransousky@insee.fr>
2022-11-23 18:24:36 +0100

    social ok

--- src/Social.js
+++ src/Social.js
@@ -5,9 +5,9 @@ import {FiGithub, FiLinkedin} from"react-icons/fi"
 function Social() {
   return (
     <section id="social">
-        <a>
-       <FiGithub className="icon"/></a>
+      <a><FiGithub className="icon"/></a>
        <a><FiLinkedin className="icon"/></a>
+       <div id="finbarre"></div>
     </section>
   )
 }
