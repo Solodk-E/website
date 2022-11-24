f7ca88f63f30effad07202972215d8c65065b40b
Elton Solodki-Fransousky <elton.solodki-fransousky@insee.fr>
2022-11-24 19:16:47 +0100

    page1 photo ok

--- src/APropos.js
+++ src/APropos.js
@@ -4,13 +4,48 @@ function APropos() {
   return (
     <div id="page1" className="page">
         <div id="apropos">
-            <div className="title">A propos</div>
+        <div className="title"><span className='spanNumber'>1.</span> A propos de moi<div className="titleLine"></div></div>
             <div id="corpus">
                 <div id="leftCorpus">
-                    <div id="description">blah blah</div>
-                    <div id="stackTechnique">react and Co</div>
+                
+                    <div id="description"><p>Qui consequat et sint dolore magna. Sint esse anim consequat deserunt anim tempor esse. Enim labore tempor consequat cillum velit aliquip. Do esse Lorem tempor nulla officia cillum consectetur nostrud. Amet ullamco incididunt aute velit velit tempor tempor. Sint laborum dolor do adipisicing ea nostrud nulla aliquip.</p>
+
+<p>Qui fugiat eu consectetur duis non quis incididunt nostrud. Culpa ex ea aliquip magna minim nulla. Excepteur dolor amet nisi Lorem eiusmod laboris incididunt adipisicing dolor.</p>
+
+<p>Nulla laborum deserunt amet qui incididunt anim quis ex aute. Non culpa tempor tempor ea laboris. Fugiat nulla magna consectetur est non sint ipsum est voluptate nulla.</p>
+
+</div>
+                    <div id="stackTechnique">
+                      <div className="colTech">
+                        <ul>
+                          <li>Javascript</li>
+                          <li>HTML CSS</li>
+                          <li>Boostrap, Ant Design</li>
+        
+                        </ul>
+                      </div>
+                      <div>
+                      <div className="colTech">
+                        <ul>
+                          <li>React JS</li>
+                          <li>React Native</li>
+                          <li>Node JS </li>
+                        </ul>
+                      </div>
+                      </div>
+                      <div className="colTech">
+                        <ul>
+                          <li>MongoDB</li>
+                          <li>Git Github Gitlab</li>
+                          <li>Agilité (Kanban, daily etc)</li>
+                        </ul>
+                      </div>
+                    </div>
                 </div>
-                <div id="rightCorpus">Ma photo</div>
+                <div id="rightCorpus">
+                  <img  id="photoProfil2" src="photoProfil.jpg" alt="Elton Solodki dev fullstack Node-React-React Native"/>
+                 <div id="cadre"></div>
+                </div>
             </div>
 
         </div>
